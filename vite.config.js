b78cb9cc537ff6ae9ac51afffba6345fc93c27bc
Vikas Tiwari <vikas.tiwari@elitmus.com>
2025-07-08 21:28:33 +0530

    Some fixes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['.pac.elitmus.com'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
-    exclude: [...configDefaults.exclude, 'e2e/*'],
   },
 });
